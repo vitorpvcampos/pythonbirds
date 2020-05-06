e933d877a7c4fd1acfe4e8f3846edc14d76d80cd
Vitor Campos <vitor.campos@engenharia.ufjf.br>
2020-05-06 13:09:34 -0300

    Created complex attribute filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Mariozam')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Vitor'
-    print(p.nome)
-    print(p.idade)
+    vitor = Pessoa(nome='Vitor')
+    mariozam = Pessoa(vitor, nome='Mariozam')
+    print(Pessoa.cumprimentar(mariozam))
+    print(id(mariozam))
+    print(mariozam.cumprimentar())
+    print(mariozam.nome)
+    print(mariozam.idade)
+    for filho in mariozam.filhos:
+        print(filho.nome)
